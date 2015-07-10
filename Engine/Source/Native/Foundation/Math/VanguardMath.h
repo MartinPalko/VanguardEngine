0b8141d75de9beb64696a9fc5007c81c24731dc3
MartinPalko <jeeprubi@hotmail.com>
2015-07-09 23:08:45 -0400

    - Got rid of "math" and "file system" folder, put types in types folder. - Added "utility" folder, now contains mathfunctions and filesystem.

--- Engine/Source/Native/Foundation/Math/VanguardMath.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "MathFunctions.h"
-#include "Vector3.h"
-#include "Vector2.h"
-#include "Quaternion.h"
-
--- Engine/Source/Native/Foundation/Types/VanguardTypes.h
+++ Engine/Source/Native/Foundation/Types/VanguardTypes.h
@@ -1,6 +1,12 @@
 #pragma once
 #include "Platform.h"
+
+#include "FilePath.h"
 #include "IntegerDefs.h"
+#include "Quaternion.h"
+#include "Rect.h"
 #include "VanguardString.h"
 #include "StringID.h"
 #include "VanguardTime.h"
+#include "Vector2.h"
+#include "Vector3.h"

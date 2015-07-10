58911b137814589bcb3b149db4d530dd9cb611da
MartinPalko <jeeprubi@hotmail.com>
2015-07-10 00:27:36 -0400

    - No longer create a default "project launcher" executable, since it's not meant to be used on it's own. - Guard against no project being defined in the project launcher, will just return 0. This shouldn't happen tho.

--- Engine/Source/Native/Executables/ProjectLauncher/ProjectLauncher.cpp
+++ Engine/Source/Native/Executables/ProjectLauncher/ProjectLauncher.cpp
@@ -2,10 +2,11 @@
 
 int main (int argc, char **argv)
 {
+#ifdef VANGUARD_PROJECT
 	Vanguard::Application::ShowConsoleWindow();
 	Vanguard::Core VanguardCore;
 	VanguardCore.Initialize(argc, argv, VANGUARD_PROJECT);
 	VanguardCore.Run();
-
+#endif
 	return 0;
 }

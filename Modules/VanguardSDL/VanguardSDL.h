4549b55f74664b2c8ec76a78867ceb9ad0e22f20
Martin Palko <jeeprubi@hotmail.com>
2016-08-14 13:28:24 +0200

    Added VanguardSDL module
    Currently just handles keeping track of the number of users of each of
    the SDL subsystems, and initializes/quits them as needed.

---
+++ Modules/VanguardSDL/VanguardSDL.h
@@ -0,0 +1,27 @@
+#include "VanguardSDL_Common.h"
+#include "Modules/IModule.h"
+#include "SDL.h"
+
+namespace Vanguard
+{
+	class VANGUARDSDL_API VanguardSDL : public IModule
+	{
+	public:
+		enum SDLModule : int32
+		{
+			Timer = SDL_INIT_TIMER,
+			Audio = SDL_INIT_AUDIO,
+			Video = SDL_INIT_VIDEO,
+			Joystick = SDL_INIT_JOYSTICK,
+			Haptic = SDL_INIT_HAPTIC,
+			GameController = SDL_INIT_GAMECONTROLLER,
+			Events = SDL_INIT_EVENTS
+		};
+
+		static void RegisterModuleUse(SDLModule module);
+		static void UnregisterModuleUse(SDLModule module);
+
+		virtual void LoadModule();
+		virtual void UnloadModule();
+	};
+}

b7daf1e450884a97927ba8eaa915554d759abf3a
Martin Palko <jeeprubi@hotmail.com>
2016-08-20 17:59:32 +0200

    Added zNear and zFar to camera
    Added a utility function for getting it's projection matrix.
    Set defualt values.

--- Engine/Source/Core/Renderer/Camera.cpp
+++ Engine/Source/Core/Renderer/Camera.cpp
@@ -5,7 +5,25 @@ namespace Vanguard
 	TYPE_DEFINITION(Camera, Entity);
 	
 	Camera::Camera() : Entity()
+		, fov (60)
+		, orthographic(false)
+		, zNear(0.1)
+		, zFar(1000)
 	{
 		transform = AddComponent<Transform>();
+	}
+
+	Matrix4x4 Camera::GetProjectionMatrix()
+	{
+		if (!orthographic)
+		{
+			return Matrix4x4::CreatePerspective(fov, orthographic, zNear, zFar);
+		}
+		else
+		{
+			// TODO: Orthographic matrix.
+			return Matrix4x4::GetIdentity();
+		}
+		
 	}
 }
--- Engine/Source/Core/Renderer/Camera.h
+++ Engine/Source/Core/Renderer/Camera.h
@@ -2,6 +2,7 @@
 
 #include "Entity/Entity.h"
 #include "Entity/Transform.h"
+#include "Types/Matrix4x4.h"
 
 namespace Vanguard
 {
@@ -15,10 +16,14 @@ namespace Vanguard
 	protected:
 		float fov;
 		bool orthographic;
+		float zNear;
+		float zFar;
 		Transform* transform;
 
 	public:
 		Transform* GetTransform() { return transform; }
+
+		Matrix4x4 GetMatrix();
 
 	};
 }

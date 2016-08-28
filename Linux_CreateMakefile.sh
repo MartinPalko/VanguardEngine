755d3a00be66ac419575f4f2235565c5999cedb7
Martin Palko <jeeprubi@hotmail.com>
2016-08-28 11:29:12 +0200

    Updated Linux_CreateMakefile.sh

--- Linux_CreateMakefile.sh
+++ Linux_CreateMakefile.sh
@@ -1,7 +1,7 @@
-cmakeExecutable=$(dirname $0)"/ThirdParty/CMake/Linux/bin/cmake"
-sourceRoot=$(dirname $0)
+sourceRoot=$PWD
+cmakeExecutable=$sourceRoot"/ThirdParty/CMake/Linux/bin/cmake"
 
-projectfileDirectory=$(dirname $0)"/Intermediate/MakeFile/"
+projectfileDirectory=$sourceRoot"/Intermediate/Makefile/"
 mkdir -p $projectfileDirectory
 cd $projectfileDirectory
-$cmakeExecutable -G "Unix Makefiles" $sourceRoot
+$cmakeExecutable -G "Unix Makefiles" $sourceRoot -DCMAKE_BUILD_TYPE=Debug

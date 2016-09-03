206c7b8269e5efba13bc1af4e79644593d2267b5
Martin Palko <jeeprubi@hotmail.com>
2016-09-03 10:46:39 +0200

    Removed includsion of window.h from platform.

--- Engine/Source/Foundation/Platforms/WindowsPlatform.h
+++ Engine/Source/Foundation/Platforms/WindowsPlatform.h
@@ -2,9 +2,6 @@
 #ifndef _VANGUARD_PLATFORM_IMPLEMENTATION
 #define _VANGUARD_PLATFORM_IMPLEMENTATION
 
-#define WIN32_LEAN_AND_MEAN
-#define NOMINMAX
-#include <Windows.h>
 #include <exception>
 
 namespace Vanguard

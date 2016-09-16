4222f3da295876d04a2477b9cbfa4a1792077d21
Martin Palko <jeeprubi@hotmail.com>
2016-09-16 23:53:29 +0200

    Added CORE_API specifier to all functions in core Directories, so they can be accessed outside of core.

--- Engine/Source/Core/Directories.h
+++ Engine/Source/Core/Directories.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "Core_Common.h"
 #include "Utility/FileSystem.h"
 
 namespace Vanguard
@@ -9,22 +10,22 @@ namespace Vanguard
 		Directories(){}
 
 	public:
-		static FilePath GetVanguardProjectsDirectory();
-		static FilePath GetVanguardRootDirectory();
-		static FilePath GetLogDirectory();
+		static CORE_API FilePath GetVanguardProjectsDirectory();
+		static CORE_API FilePath GetVanguardRootDirectory();
+		static CORE_API FilePath GetLogDirectory();
 
-		static FilePath GetEngineBinDirectory();
-		static FilePath GetEngineModuleDirectory();
-		static FilePath GetEngineDirectory();
-		static FilePath GetEngineConfigDirectory();
-		static FilePath GetEngineResourceDirectory();
+		static CORE_API FilePath GetEngineBinDirectory();
+		static CORE_API FilePath GetEngineModuleDirectory();
+		static CORE_API FilePath GetEngineDirectory();
+		static CORE_API FilePath GetEngineConfigDirectory();
+		static CORE_API FilePath GetEngineResourceDirectory();
 
-		static FilePath GetProjectTempDirectory();
-		static FilePath GetProjectDirectory();
-		static FilePath GetProjectConfigDirectory();
-		static FilePath GetProjectModuleDirectory();
-		static FilePath GetProjectResourceDirectory();
+		static CORE_API FilePath GetProjectTempDirectory();
+		static CORE_API FilePath GetProjectDirectory();
+		static CORE_API FilePath GetProjectConfigDirectory();
+		static CORE_API FilePath GetProjectModuleDirectory();
+		static CORE_API FilePath GetProjectResourceDirectory();
 
-		static DynamicArray<FilePath> GetResourceDirectories();
+		static CORE_API DynamicArray<FilePath> GetResourceDirectories();
 	};
 }

befa92cd7cf439f564fe7ef11c0d696b88b187e1
Martin Palko <jeeprubi@hotmail.com>
2016-09-30 21:36:15 +0200

    Implemented basics of LogViewer

--- Editor/Source/Widgets/LogViewer.cpp
+++ Editor/Source/Widgets/LogViewer.cpp
@@ -1,8 +1,48 @@
 #include "LogViewer.h"
 
+#include <QTextEdit>
+#include <QScrollBar>
+
 namespace Vanguard
 {
 	LogViewer::LogViewer(QWidget* parent) : QDockWidget("Log", parent)
+		, autoScroll(true)
 	{
+		textWidget = new QTextEdit(this);
+		textWidget->setReadOnly(true);
+		
+		setWidget(textWidget);
+
+		Log::RegisterListener(this);
+	}
+
+	LogViewer::~LogViewer()
+	{
+		Log::UnregisterListener(this);
+	}
+
+	void LogViewer::OnMessageLogged(LogEntry aMessage)
+	{
+		switch (aMessage.GetWarningLevel())
+		{
+		case LogEntryErrorLevel::Warning:
+			textWidget->setTextColor(QColor(255, 220, 0));
+			break;
+		case LogEntryErrorLevel::Error:
+		case LogEntryErrorLevel::Exception:
+			textWidget->setTextColor(QColor(255, 0, 0));
+			break;
+		default:
+		case LogEntryErrorLevel::Message:
+			textWidget->setTextColor(QColor(0, 0, 0));
+			break;
+		}
+
+		textWidget->append(aMessage.GetFormattedLogEntry().GetCharPointer());
+
+		if (autoScroll)
+		{
+			textWidget->verticalScrollBar()->setValue(textWidget->verticalScrollBar()->maximum());
+		}
 	}
 }
--- Editor/Source/Widgets/LogViewer.h
+++ Editor/Source/Widgets/LogViewer.h
@@ -3,13 +3,22 @@
 
 #include <QDockWidget>
 
+class QTextEdit;
+
 namespace Vanguard
 {
-	class LogViewer : public QDockWidget
+	class LogViewer : public QDockWidget, ILogListener
 	{
 		Q_OBJECT;
 
+		QTextEdit* textWidget;
+		bool autoScroll;
+
 	public:
 		LogViewer(QWidget* parent);
+		~LogViewer();
+
+		// Implement ILogListener
+		void OnMessageLogged(LogEntry aMessage) override;
 	};
 }

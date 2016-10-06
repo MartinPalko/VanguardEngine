16c5f70156b71491b8883ecd85f269b44ae31f13
Martin Palko <jeeprubi@hotmail.com>
2016-10-06 23:55:03 +0200

    Inspector now reacts to selected widget
    Currently just sets title as a test

--- Editor/Source/Widgets/Inspector.cpp
+++ Editor/Source/Widgets/Inspector.cpp
@@ -1,8 +1,29 @@
 #include "Inspector.h"
+#include "EditorEvents.h"
 
 namespace Vanguard
 {
-	Inspector::Inspector(QWidget* parent) : QDockWidget("Inspector", parent)
+	void Inspector::OnEditorEvent(EditorEvent* aEvent)
 	{
+		if (EntitySelectedEvent* selectedEvent = Type::SafeCast<EntitySelectedEvent>(aEvent))
+		{
+			SetEntity(selectedEvent->GetEntity());
+		}
+	}
+
+	Inspector::Inspector(QWidget* parent) : QDockWidget("Inspector", parent)
+		, entity(nullptr)
+	{
+		EditorCore::GetInstance()->RegisterEventListener(this);
+	}
+
+	Inspector::~Inspector()
+	{
+		EditorCore::GetInstance()->UnregisterEventListener(this);
+	}
+
+	void Inspector::SetEntity(Entity * aEntity)
+	{
+		setWindowTitle(("Inspector: " + aEntity->GetType()->GetTypeName()).GetCharPointer());
 	}
 }
--- Editor/Source/Widgets/Inspector.h
+++ Editor/Source/Widgets/Inspector.h
@@ -1,15 +1,23 @@
 #pragma once
 #include "Editor_Common.h"
+#include "EditorCore.h"
 
 #include <QDockWidget>
 
 namespace Vanguard
 {
-	class Inspector : public QDockWidget
+	class Inspector : public QDockWidget , public IEditorEventListener
 	{
 		Q_OBJECT;
 
+		Entity* entity;
+
+		// Implement IEditorEventListener
+		virtual void OnEditorEvent(EditorEvent* aEvent) override;
 	public:
 		Inspector(QWidget* parent);
+		~Inspector();
+		
+		void SetEntity(Entity* aEntity);
 	};
 }

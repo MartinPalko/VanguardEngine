2839702102a481a0fba06bfe96e7eeb29ab3dd52
Martin Palko <jeeprubi@hotmail.com>
2016-10-11 23:50:01 +0200

    Stylemanager now applied font, and handles realtive paths in qss files.

--- Editor/Source/StyleManager.cpp
+++ Editor/Source/StyleManager.cpp
@@ -7,6 +7,8 @@
 #include <QString>
 #include <QFile>
 #include <QDir>
+#include <QFont>
+
 #include "sass.h"
 
 QString LoadStyleSheet(Vanguard::FilePath aFilePath, bool aSass)
@@ -14,41 +16,49 @@ QString LoadStyleSheet(Vanguard::FilePath aFilePath, bool aSass)
 	if (!Vanguard::FileSystem::FileExists(aFilePath))
 		return "";
 
-	if (!aSass)
+	QString output;
+
+	if (aSass)
+	{
+		Sass_File_Context* context = sass_make_file_context(aFilePath.GetFullPathName().GetCharPointer());
+		Sass_Options* options = sass_file_context_get_options(context);
+		sass_option_set_precision(options, 1);
+		sass_option_set_source_comments(options, true);
+
+		sass_file_context_set_options(context, options);
+
+		Sass_Context* sass_context = sass_file_context_get_context(context);
+
+		Sass_Compiler* compiler = sass_make_file_compiler(context);
+		sass_compiler_parse(compiler);
+		sass_compiler_execute(compiler);
+
+		output = sass_context_get_output_string(sass_context);
+
+		// Retrieve errors during compilation
+		int status = sass_context_get_error_status(sass_context);
+		const char* error = sass_context_get_error_text(sass_context);
+
+		if (status)
+		{
+			LOG_ERROR(error, "scss");
+		}
+
+		// Release memory dedicated to the C compiler
+		sass_delete_compiler(compiler);
+		sass_delete_file_context(context);
+	}
+	else
 	{
 		QFile styleSheetFile(aFilePath.GetFullPathName().GetCharPointer());
 		styleSheetFile.open(QFile::ReadOnly);
-		QString fileContents = QLatin1String(styleSheetFile.readAll());
-		return fileContents;
+		output = QLatin1String(styleSheetFile.readAll());
 	}
 
-	Sass_File_Context* context = sass_make_file_context(aFilePath.GetFullPathName().GetCharPointer());
-	Sass_Options* options = sass_file_context_get_options(context);
-	sass_option_set_precision(options, 1);
-	sass_option_set_source_comments(options, true);
-
-	sass_file_context_set_options(context, options);
-
-	Sass_Context* sass_context = sass_file_context_get_context(context);
-
-	Sass_Compiler* compiler = sass_make_file_compiler(context);
-	sass_compiler_parse(compiler);
-	sass_compiler_execute(compiler);
-
-	QString output = sass_context_get_output_string(sass_context);
-
-	// Retrieve errors during compilation
-	int status = sass_context_get_error_status(sass_context);
-	const char* error = sass_context_get_error_text(sass_context);
-
-	if (status)
-	{
-		LOG_ERROR(error, "scss");
-	}
-
-	// Release memory dedicated to the C compiler
-	sass_delete_compiler(compiler);
-	sass_delete_file_context(context);
+	// Parse and replace all local url references with absolute paths.
+	QString directory = aFilePath.GetParentDirectory().GetFullPathName().GetCharPointer();
+	directory.replace("\\", "/");
+	output.replace("url(./", "url(" + directory + "/", Qt::CaseInsensitive);
 
 	return output;
 }
@@ -67,8 +77,11 @@ StyleManager* StyleManager::GetInstance()
 void StyleManager::LoadStyle()
 {
 	Vanguard::FilePath styleDirectory = Vanguard::EditorDirectories::GetStyleDirectory();
-	QDir::addSearchPath("", styleDirectory.GetFullPathName().GetCharPointer());
+	QDir::addSearchPath("icons", styleDirectory.GetRelative("Icons").GetFullPathName().GetCharPointer());
 
 	Vanguard::FilePath styleSheetPath = styleDirectory.GetRelative("EditorStyle.scss");
-	((QApplication*)QApplication::instance())->setStyleSheet(LoadStyleSheet(styleSheetPath, true));	
+	((QApplication*)QApplication::instance())->setStyleSheet(LoadStyleSheet(styleSheetPath, true));
+
+	QFont newFont("Verdana", 9, 50);
+	QApplication::setFont(newFont);
 }
